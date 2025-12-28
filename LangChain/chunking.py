6deeee7a227cd96dc340e2ba342a0da6e2236a31
Ricardo Palacios <rpmaya@gmail.com>
2025-12-28 19:11:22 +0100

    Update chunking.py

--- LangChain/chunking.py
+++ LangChain/chunking.py
@@ -1,4 +1,4 @@
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 # Crear splitter con configuración
 splitter = RecursiveCharacterTextSplitter(
